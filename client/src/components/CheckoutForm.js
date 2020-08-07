728b0c519a179d5f01557be2a8e80350db02c0a8
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-07 13:08:15 -0400

    Add ids for testing

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -33,6 +33,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
+            data-testid="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
@@ -41,6 +42,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
+            data-testid="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
@@ -49,6 +51,7 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
+            data-testid="address"
             name="address"
             value={values.address}
             onChange={handleChanges}
@@ -56,15 +59,24 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input data-testid="city"
+                 name="city"
+                 value={values.city}
+                 onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input data-testid="state"
+                 name="state"
+                 value={values.state}
+                 onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input data-testid="zip"
+                 name="zip"
+                 value={values.zip}
+                 onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
