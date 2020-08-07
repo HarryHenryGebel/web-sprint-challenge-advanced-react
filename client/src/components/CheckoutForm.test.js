b4b1348f6fce45fefa604ed2d75acb7c484e32fe
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-07 13:34:11 -0400

    Yes, I said it. Don't but in these stupid spaces.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -21,12 +21,12 @@ test("form shows success message on submit with form details", async() => {
         submitButton = screen.getByTestId("submitButton");
 
   // fill out the form
-  await fireEvent.change(firstName, { target: { value: "Harry" } });
-  await fireEvent.change(lastName, { target: { value: "Gebel" } });
-  await fireEvent.change(address, { target: { value: "13300 Walsingham RD" } });
-  await fireEvent.change(city, { target: { value: "Largo" } });
-  await fireEvent.change(state, { target: { value: "Florida" } });
-  await fireEvent.change(zip, { target: { value: "37744" } });
+  await fireEvent.change(firstName, {target: {value: "Harry"}});
+  await fireEvent.change(lastName, {target: {value: "Gebel"}});
+  await fireEvent.change(address, {target: {value: "13300 Walsingham RD"}});
+  await fireEvent.change(city, {target: {value: "Largo"}});
+  await fireEvent.change(state, {target: {value: "Florida"}});
+  await fireEvent.change(zip, {target: {value: "37744"}});
 
   // Submit form
   await fireEvent.click(submitButton);
