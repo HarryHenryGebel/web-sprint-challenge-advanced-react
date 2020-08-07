40387a791c469861a96a701024e03d38734a95f7
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-07 13:52:24 -0400

    Move all form logic into hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 
 export default function useForm(initialValue){
-    const [values, setValues] = useState(initialValue);
-    const handleChanges = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value });
-      };
+  const [values, setValues] = useState(initialValue),
+        [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  return [values, handleChanges];
+  function handleChanges(event) {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Make sure all the fields are filled in
+    setShowSuccessMessage(values.firstName &&
+                          values.lastName &&
+                          values.address &&
+                          values.city &&
+                          values.zip);
+  };
+
+  return [values, showSuccessMessage, handleChanges, handleSubmit];
 }
